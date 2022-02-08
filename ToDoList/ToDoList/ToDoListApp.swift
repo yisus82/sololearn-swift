71244513fb6b532ac5c613b55d811473eeff40aa
yisus82 <djalma8@gmail.com>
2022-02-08 09:52:30 +0100

    Initial template files

---
+++ ToDoList/ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Jesús Ángel Pérez-Roca Fernández on 8/2/22.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
